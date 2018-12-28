bfb2f84be00223edbb6edb47ca14d90d612517bd
zombietux <zombietux@Macui-MacBook-Pro-2.local>
2018-12-28 22:45:56 +0900

    tetris game init & Array2D

---
+++ TetrisApp/Array2D.swift
@@ -0,0 +1,30 @@
+//
+//  Array2D.swift
+//  TetrisApp
+//
+//  Created by zombietux on 28/12/2018.
+//  Copyright © 2018 zombietux. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+
+        array = Array<T?>(repeating: nil, count:rows * columns)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
